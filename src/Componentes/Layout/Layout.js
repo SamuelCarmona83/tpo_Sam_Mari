4b37f440cd4e24a3af398300a1f7ad5b681d4bc5
Samuel <samg.martinez2@gmail.com>
2024-09-16 03:15:19 -0300

    Se agrego la estructura basica de la pagina cuando el usuario se loguea

--- src/Componentes/Layout/Layout.js
+++ src/Componentes/Layout/Layout.js
@@ -6,23 +6,16 @@ import Navbar from './Navbar';
 
 
 function Layout (){
-    const [estadoLog, setEstadoLog] = useState(false);
-
-    const cambiarFondo = () => {
-        setEstadoLog(!estadoLog);
-    }
-
+    
     return (
         <div>
             <header >
                 <nav className='logOfNavBar'>
-                    <div className={estadoLog ? 'f0' : 'f1'} >
-                        <div className="divNav row_spaceBetween">
-                            <figure >
-                                    <img className='logoNav' src={logo} alt='Logo de la pagina, es un gato'/>
-                            </figure>
-                            <Navbar />
-                        </div>
+                    <div className="divNav row_spaceBetween">
+                        <figure >
+                                <img className='logoNav' src={logo} alt='Logo de la pagina, es un gato'/>
+                        </figure>
+                        <Navbar />
                     </div>
                 </nav>
             </header>
