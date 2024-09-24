2aa30701d2148484636550f4afdfcabaf0204702
MariangelVillegas <mariangelarbona96@gmail.com>
2024-09-23 23:45:59 -0300

    diseño del footer

--- src/Componentes/Usuario/Participantes.js
+++ src/Componentes/Usuario/Participantes.js
@@ -5,6 +5,7 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import './style.css';
 
 export default function Participantes(props) {
   return (
@@ -13,12 +14,7 @@ export default function Participantes(props) {
         <AccordionSummary
           expandIcon={<ArrowDownwardIcon />}
           aria-controls="panel1-content"
-          id="panel1-header"
-        >
-          <img src={props.imagen} alt={props.nombre} className="participante-imagen"
-          />
-         
-          <Typography> {props.nombre}</Typography>
+          id="panel1-header" className='itemsAccodion'> <div id="datosParticipante"> <img src={props.imagen} alt={props.nombre} className="participante-imagen"/> <p id="nombre-participante">{props.nombre}</p> </div>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
--- src/Componentes/Usuario/Proyecto.js
+++ src/Componentes/Usuario/Proyecto.js
@@ -5,44 +5,45 @@ import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import Participantes from './Participantes';
 import imagen1 from '../../imagenes/mari.jpeg';
 import imagen2 from '../../imagenes/samu.jpeg';
+import imagen3 from '../../imagenes/corazon.png';
 
 
 
 
 const proyectos = [{
-    nombre:"proyecto pepito",
+    nombre: "proyecto pepito",
     ID: 0,
-    partici: [0,1,3]
-},{
-    nombre:"proyecto 2",
+    partici: [0, 1, 3]
+}, {
+    nombre: "proyecto 2",
     ID: 1,
-    partici: [0,1,3]
+    partici: [0, 1, 3]
 
-},{
-    nombre:"proyecto 3",
+}, {
+    nombre: "proyecto 3",
     ID: 2,
-    partici: [0,1]
+    partici: [0, 1]
 }];
 
 const participantes = [
     {
-      participante: <Participantes imagen= {imagen1} nombre="Mariangel" ID="001" collap="#collapseOne" monto="300" />
+        participante: <Participantes imagen={imagen1} nombre="Mariangel" ID="001" collap="#collapseOne" monto="300" />
     },
     {
-      participante: <Participantes imagen= {imagen2} nombre="Samuel" ID="002" collap="#collapseTwo" monto="500"/>
+        participante: <Participantes imagen={imagen2} nombre="Samuel" ID="002" collap="#collapseTwo" monto="500" />
     },
     {
-      participante: <Participantes nombre="Jose Alejandro" ID="003" collap= "#collapseThree" />
+        participante: <Participantes nombre="Jose Alejandro" ID="003" collap="#collapseThree" />
     },
     {
-      participante: <Participantes nombre="Eddymar Orellana" ID="004"></Participantes>
+        participante: <Participantes imagen={imagen3} nombre="Eddymar Orellana" ID="004"></Participantes>
     }
-  ];
-  
+];
 
 
 
-function Proyecto ({proyectoID}) {
+
+function Proyecto({ proyectoID }) {
     const [alignment, setAlignment] = React.useState('datos');
 
     const handleChange = (event, newAlignment) => {
@@ -50,25 +51,25 @@ function Proyecto ({proyectoID}) {
     };
 
     let headerProyecto;
-    if (proyectoID != 'n'){
+    if (proyectoID != 'n') {
         let titulo = proyectos[proyectoID].nombre;
         headerProyecto = (
-                <div id='navProyecto'>
-                    <h2>{titulo}</h2>
-                    <ToggleButtonGroup
-                        color="primary"
-                        value={alignment}
-                        exclusive
-                        onChange={handleChange}
-                        aria-label="Platform"
-                        >
-                        <ToggleButton value="datos">Datos</ToggleButton>
-                        <ToggleButton value="participantes" >Participantes</ToggleButton>
-                        <ToggleButton value="configuración">Configuración</ToggleButton>
-                    </ToggleButtonGroup>
-                </div>
+            <div id='navProyecto'>
+                <h2>{titulo}</h2>
+                <ToggleButtonGroup
+                    color="primary"
+                    value={alignment}
+                    exclusive
+                    onChange={handleChange}
+                    aria-label="Platform"
+                >
+                    <ToggleButton value="datos">Datos</ToggleButton>
+                    <ToggleButton value="participantes" >Participantes</ToggleButton>
+                    <ToggleButton value="configuración">Configuración</ToggleButton>
+                </ToggleButtonGroup>
+            </div>
         );
-    }else {
+    } else {
         headerProyecto = (
             <h2>Bienvenidos, elija un proyecto!</h2>
         );
@@ -78,8 +79,8 @@ function Proyecto ({proyectoID}) {
     let main;
 
     console.log(alignment);
-    if(proyectoID != 'n'){
-        switch (alignment){
+    if (proyectoID != 'n') {
+        switch (alignment) {
             case 'datos':
                 main = (
                     <h1>datos</h1>
@@ -97,7 +98,7 @@ function Proyecto ({proyectoID}) {
                     <h1>configuración</h1>
                 )
                 break
-            }
+        }
     }
 
     return (
