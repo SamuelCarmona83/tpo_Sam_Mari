854f16ff52e8ec8748234432021cefafae148180
MariangelVillegas <mariangelarbona96@gmail.com>
2024-09-26 21:31:13 -0300

    Funcion agregar participante proyecto

--- src/Componentes/Usuario/Proyecto.js
+++ src/Componentes/Usuario/Proyecto.js
@@ -2,14 +2,66 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
-import {getProyectobyID} from '../../Backend/BD';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { getProyectobyID, getUsuarios, agregarUsuario, agregarParticipante, getProyectos } from '../../Backend/BD';
 import ParticipantesList from './ParticipantesList';
 import InfoProyecto from './InformacionProyecto';
+import imagen4 from '../../imagenes/agregarUsuario.png';
+import imagen5 from '../../imagenes/mari.jpeg';
+
 
 
 function Proyecto({ proyectoID }) {
+    let proyectos = getProyectos();
+    console.log(proyectos);
+    
+    
+    const [usuarios, setUsuarios] = React.useState(getUsuarios());
+    const [cantidad, setCantidad] = React.useState(usuarios.length || 0);
+    const [open, setOpen] = React.useState(false);
+    const [nombre, setNombreUsuario] = React.useState('');
+
+    const eliminarParticipante = () => {
+        setCantidad(cantidad - 1);
+    };
+
+    const abrirFormulario = () => {
+        setOpen(true);
+    };
+
+    const botonCerrar = () => {
+        setOpen(false);
+    };
+
+    const crearNuevoUsuario = () => {
+        const nuevoUsuario = {
+            ID: String(cantidad + 1).padStart(3, '0'),
+            nombre: nombre,
+            imagen: '../../imagenes/mari.jpeg',
+            monto: '300'
+        };
+
+
+        agregarUsuario(nuevoUsuario);
+        setUsuarios(prevUsuarios => [...prevUsuarios, nuevoUsuario]);
+        agregarParticipante(proyectoID, usuarios.length-1);
+        
+        botonCerrar();
+        setNombreUsuario('');
+        setCantidad(cantidad + 1);
+        
+        
+
+    };
+    
+
     let ProySeleccionado;
-    if(proyectoID != 'n'){
+    if (proyectoID !== 'n') {
         ProySeleccionado = getProyectobyID(proyectoID);
     }
 
@@ -20,7 +72,7 @@ function Proyecto({ proyectoID }) {
     };
 
     let headerProyecto;
-    if (proyectoID !== 'n'){
+    if (proyectoID !== 'n') {
         headerProyecto = (
             <div id='navProyecto'>
                 <h2>{ProySeleccionado.nombre}</h2>
@@ -29,10 +81,10 @@ function Proyecto({ proyectoID }) {
                     value={alignment}
                     exclusive
                     onChange={handleChange}
-                    aria-label="Platform"
+                    aria-label="Plataforma"
                 >
                     <ToggleButton value="datos">Datos</ToggleButton>
-                    <ToggleButton value="participantes" >Participantes</ToggleButton>
+                    <ToggleButton value="participantes">Participantes</ToggleButton>
                     <ToggleButton value="configuración">Configuración</ToggleButton>
                 </ToggleButtonGroup>
             </div>
@@ -45,28 +97,20 @@ function Proyecto({ proyectoID }) {
         );
     }
 
-
     let main;
 
-    if(proyectoID !== 'n'){
-        switch (alignment){
+    if (proyectoID !== 'n') {
+        switch (alignment) {
             case 'datos':
-                main = (
-                    <InfoProyecto />
-                )
-                break
-            case 'participantes': 
-                main = (
-                    <ParticipantesList 
-                        proyectoID={proyectoID} 
-                    />
-                );
+                main = <InfoProyecto />;
+                break;
+            case 'participantes':
+                main = <ParticipantesList proyectoID={proyectoID} abrir= {abrirFormulario} />;
+
                 break;
             default:
-                main = (
-                    <h1>configuración</h1>
-                )
-            }
+                main = <h1>Configuración</h1>;
+        }
     }
 
     return (
@@ -74,7 +118,33 @@ function Proyecto({ proyectoID }) {
             {headerProyecto}
             <div id='mainProyecto'>
                 {main}
+
             </div>
+            <Dialog open={open} onClose={botonCerrar}>
+                <DialogTitle>Agregar Participante</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="nombreParticipante"
+                        label="Nombre participante"
+                        type="text"
+                        fullWidth
+                        variant="outlined"
+                        value={nombre}
+                        onChange={(e) => setNombreUsuario(e.target.value)}
+                    />
+
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={botonCerrar} color="primary">
+                        Cancelar
+                    </Button>
+                    <Button onClick={() => crearNuevoUsuario(nombre)} color="primary">
+                        Agregar
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </article>
     );
 }
